12fd097916a2b0bad812f19841ded0a7dfb4f974
yung kahuna <tjfaustino7@gmail.com>
2019-01-18 16:52:08 -1000

    six seven eight

--- exercises.js
+++ exercises.js
@@ -107,7 +107,7 @@ var stockCar = {
     driver: null,
     passengers: []
 };
-console.log(stockCar);
+//console.log(stockCar);
 
 /*
 
@@ -125,8 +125,14 @@ console.log(stockCar);
    the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
    the value at `name`, and just the value at `age`.
  */
-
-
+var plainPerson = {};
+ function buildPerson(person,nameString,age){
+     person.name = nameString;
+     person.age = age;
+     return person;
+ }
+var completePerson = buildPerson(plainPerson, 'yo mom', 25);
+//console.log(completePerson);
 /*
 7. Display values of objects that are inside an array
    Declare a new variable named `arrayOfObjects` and set it to be [this array of objects
@@ -152,7 +158,67 @@ console.log(stockCar);
             ...
  */
 
+var arrayOfObjects = [{
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }];
 
+  function printOrders(orders){
+      for(var i = 0; i<orders.length; i++){
+      console.log('=====');
+      console.log('id: ' + orders[i].id);
+      console.log('date: ' + orders[i].date);
+      console.log('total: ' + orders[i].total);
+  }
+}
+
+  printOrders(arrayOfObjects);
 /*
 8. Addition with an object
    Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. 
@@ -166,8 +232,20 @@ console.log(stockCar);
         to inspect your results.
 */
 
+var sumObj = {
+    a: 2,
+    b: 4,
+    result: undefined
+};
 
-/*
+function objectAddition(object){
+    object.result = object.a + object.b;
+    return object;
+}
+
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
+    /*
 9. Print sum function and add as new key-value
    Declare a new function named printObj and a single parameter which will be the object from the challenge just above. 
    Within this function you are to print to the screen a message of the operation performed. For Example:
